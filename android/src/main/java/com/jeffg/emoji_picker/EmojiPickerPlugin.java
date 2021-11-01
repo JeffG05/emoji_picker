807bf6be1ecca53dc0d2ccddc9910a64d850a4f6
linhhoangtm <59860436+linhhoangtm@users.noreply.github.com>
2021-11-01 16:04:01 +0700

    Update EmojiPickerPlugin.java

--- android/src/main/java/com/jeffg/emoji_picker/EmojiPickerPlugin.java
+++ android/src/main/java/com/jeffg/emoji_picker/EmojiPickerPlugin.java
@@ -1,13 +1,12 @@
 package com.jeffg.emoji_picker;
 
 import androidx.annotation.NonNull;
-
+import android.content.Context;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import android.graphics.Paint;
 import androidx.core.graphics.PaintCompat;
@@ -17,30 +16,13 @@ import java.util.Map;
 
 /** EmojiPickerPlugin */
 public class EmojiPickerPlugin implements FlutterPlugin, MethodCallHandler {
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    final MethodChannel channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(),
-        "emoji_picker");
-    channel.setMethodCallHandler(new EmojiPickerPlugin());
-  }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It
-  // supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new
-  // Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith
-  // to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith
-  // will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both
-  // be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "emoji_picker");
-    channel.setMethodCallHandler(new EmojiPickerPlugin());
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(), "emoji_picker");
+    channel.setMethodCallHandler(this);
   }
 
   @Override
@@ -65,5 +47,7 @@ public class EmojiPickerPlugin implements FlutterPlugin, MethodCallHandler {
 
   @Override
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+    channel = null;
   }
 }
